b87ef326e75676bf3f20b9b555d0db99915c886c
Alibek <alibekbidaulet2003@gmail.com>
2022-08-08 18:15:15 +0600

    first commit

--- src/domain/memes.js
+++ src/domain/memes.js
@@ -1,12 +1,17 @@
 const memes = [
     {
         "name":"hasbulla",
-        "image":"https://img-9gag-fun.9cache.com/photo/aZrzY4z_460s.jpg",
+        "image":"https://sun9-86.userapi.com/impf/c637221/v637221333/641e8/QXR2sdH4BSw.jpg?size=544x316&quality=96&sign=70753e48407039ac1ba223665b43b37d&type=album",
         "translation":"Мен үшінші дүниежүзілік соғыстан кейін Хасбулламен бірге ұшып бара жатырмын"
     },
     {
         "name":"hasbulla",
-        "image":"https://static.caravan.kz/image/landscape/1200/636716.jpg",
+        "image":"https://sun9-78.userapi.com/impf/c840331/v840331164/6c9d3/TxmeV4EOHQ8.jpg?size=512x600&quality=96&sign=463b03c646ccf2b5f81c1807d2ffc538&type=album",
+        "translation":"lorem"
+    },
+    {
+        "name":"hasbulla",
+        "image":"https://sun9-54.userapi.com/impf/c846523/v846523334/d448/KApoANafoKQ.jpg?size=604x574&quality=96&sign=c9f679b317783490f4fa3c9f02e99d08&type=album",
         "translation":"lorem"
     }
 ]
